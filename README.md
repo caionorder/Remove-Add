573610fcfb41787d91345879a86c8261f7636843
Caio Norder <caio@caionorder.com>
2014-12-10 08:56:01 -0200

    Plugin Wordpress

---
+++ README.md
@@ -0,0 +1,6 @@
+# Remove Add - Wordpress Plugin
+This plugin removes the add button again within the web page in the post.
+
+## Install
+Add in your wp-content/plugins/ and active same.
+
---
+++ remove-add.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Remove Add 
+ * @version 1.0
+ */
+/*
+Plugin Name: Remove Add 
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: This plugin removes the add button again within the web page in the post.
+Author: Caio Norder
+Version: 1.0
+Author URI: http://caionorder.com/
+*/
+
+// We need some CSS to position the paragraph
+function remove_add_new() {
+
+	echo "
+	<style type='text/css'>
+	.add-new-h2 {
+		display:none !important;
+	}
+	</style>
+	";
+}
+
+add_action( 'admin_head', 'remove_add_new' );
+
+?>
